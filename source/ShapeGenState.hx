2e5cf2b4ac48bffaa1c420e89cf45ce17d721d7c
Vulpicula <ErisOT@protonmail.com>
2022-01-18 20:25:52 -0800

    Final work! Really happy.

--- source/ShapeGenState.hx
+++ source/ShapeGenState.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.math.FlxPoint;
+import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 
@@ -11,7 +12,13 @@ using flixel.util.FlxSpriteUtil;
 
 class ShapeGenState extends FlxState
 {
+	// UI related.
 	var isDrawing:Bool = true;
+
+	var chosenColour:Int = 0;
+	var hideUI = false;
+	var quickDraw = false;
+	var rainbowMode = false;
 
 	var canvasSprite = new FlxSprite();
 	var lineStyle:LineStyle;
@@ -21,11 +28,13 @@ class ShapeGenState extends FlxState
 	var shapePointB:FlxPoint;
 
 	var shapeRadius:Int = 300;
-	var shapeSides:Int = 200;
+	var shapeSides:Int = 8;
 
 	var drawIterations:Int = 0;
 
-	var drawMode:Int = 2;
+	var drawMode:Int = 0;
+
+	var infoText:FlxText;
 
 	override public function create()
 	{
@@ -40,16 +49,145 @@ class ShapeGenState extends FlxState
 		shapePointB = new FlxPoint(shapePointA.x, shapePointA.y);
 		shapePointB.rotate(shapeCenterCoordinate, 360 / shapeSides); // Rotates B.
 
-		// Start a timer. Upon each conclusion it resets the timer and draws a line.
+		// On-screen information.
+		infoText = new FlxText(1, 1, 0,
+			"([ShapeGen Controls])\n > [C] to toggle colour. \n > [M] to toggle drawing mode.\n > [Q] to enable quick drawing. \n > [R] to toggle RAINBOWS (overwrites [C])." +
+			"\n > [+] to increase sides.\n > [-] to decrease sides (min 3). \n > [Shift] to increase radius by 15px (max 450).\n > [Ctrl] to decrease radius by 15px (min 15)." +
+			"\n > [ESC] to redraw with changes.\n > \n > [H] to hide this text.",
+			16); // Start a timer. Upon each conclusion it resets the timer and draws a line.
+		infoText.color = FlxColor.GRAY;
+		add(infoText);
+
 		// This is my attempt at emulating what Snap does in a simple way.
 		trace("Starting timer...");
-		new FlxTimer().start(2 / shapeSides, timerComplete, 0);
+		new FlxTimer().start(1 / shapeSides, timerComplete, 0);
 
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		if (isDrawing == false)
+		{
+			if (FlxG.keys.anyJustPressed([C]))
+			{
+				chosenColour++;
+				rainbowMode = false;
+
+				trace("chosenColour set to " + chosenColour);
+				if (chosenColour > 7)
+				{
+					chosenColour = 0;
+				}
+				setLineColour(chosenColour);
+				if (quickDraw == true)
+				{
+					resetCanvas();
+				}
+			}
+			if (FlxG.keys.anyJustPressed([H]))
+			{
+				trace("Toggling UI.");
+				if (hideUI == false)
+				{
+					remove(infoText);
+					hideUI = true;
+				}
+				else
+				{
+					add(infoText);
+					hideUI = false;
+				}
+			}
+			if (FlxG.keys.anyJustPressed([M]))
+			{
+				trace("Toggling drawMode.");
+				drawMode++;
+				if (drawMode > 3)
+				{
+					drawMode = 0;
+				}
+				if (quickDraw == true)
+				{
+					resetCanvas();
+				}
+			}
+			if (FlxG.keys.anyJustPressed([Q]))
+			{
+				trace("Toggling instantDraw.");
+				if (quickDraw == false)
+				{
+					quickDraw = true;
+				}
+				else
+				{
+					quickDraw = false;
+				}
+				resetCanvas();
+			}
+			if (FlxG.keys.anyJustPressed([R]))
+			{
+				if (rainbowMode == false)
+				{
+					trace("RAINBOWS activated.");
+					rainbowMode = true;
+				}
+				else
+				{
+					trace("RAINBOWS disabled :(.");
+					rainbowMode = false;
+				}
+				if (quickDraw == true)
+				{
+					resetCanvas();
+				}
+			}
+			if (FlxG.keys.anyJustPressed([PLUS]))
+			{
+				shapeSides++;
+				if (quickDraw == true)
+				{
+					resetCanvas();
+				}
+			}
+			if (FlxG.keys.anyJustPressed([MINUS]))
+			{
+				if (shapeSides > 3)
+				{
+					shapeSides--;
+				}
+				if (quickDraw == true)
+				{
+					resetCanvas();
+				}
+			}
+			if (FlxG.keys.anyJustPressed([SHIFT]))
+			{
+				if (shapeRadius <= 450)
+				{
+					shapeRadius += 15;
+				}
+				if (quickDraw == true)
+				{
+					resetCanvas();
+				}
+			}
+			if (FlxG.keys.anyJustPressed([CONTROL]))
+			{
+				if (shapeRadius >= 15)
+				{
+					shapeRadius -= 15;
+				}
+				if (quickDraw == true)
+				{
+					resetCanvas();
+				}
+			}
+			if (FlxG.keys.anyJustPressed([ESCAPE]))
+			{
+				resetCanvas();
+			}
+		}
 		super.update(elapsed);
 	}
 
@@ -61,7 +199,9 @@ class ShapeGenState extends FlxState
 		drawIterations++;
 		trace("Iteration: " + drawIterations + "/" + shapeSides);
 
-		if (shapePointA.x >= 720 && shapePointA.y >= 150 && drawIterations >= shapeSides) // Requires two passed checks, otherwise it ends early for some reason.
+		if (shapePointA.x >= shapeCenterCoordinate.x
+			&& shapePointA.y >= shapeCenterCoordinate.y - shapeRadius
+			&& drawIterations >= shapeSides) // Requires three passed checks, otherwise it ends early for some reason.
 		{
 			isDrawing = false;
 			timer.destroy();
@@ -72,7 +212,16 @@ class ShapeGenState extends FlxState
 	{
 		trace("shapePointA = " + shapePointA);
 		trace("shapePointB = " + shapePointB);
-		canvasSprite.drawLine(shapePointA.x, shapePointA.y, shapePointB.x, shapePointB.y);
+		if (rainbowMode == true)
+		{
+			chosenColour++;
+			if (chosenColour > 7)
+			{
+				chosenColour = 1;
+			}
+			setLineColour(chosenColour);
+		}
+		canvasSprite.drawLine(shapePointA.x, shapePointA.y, shapePointB.x, shapePointB.y, lineStyle);
 		// Rotate both points by the shapeAngle.
 		if (drawMode != 2)
 		{
@@ -92,19 +241,74 @@ class ShapeGenState extends FlxState
 			canvasSprite.destroy(); // Don't destroy this if it doesn't actually exist.
 		}
 		// Remake sprite.
+		canvasSprite = new FlxSprite();
 		canvasSprite.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK, true);
 		add(canvasSprite);
+		// Removes text, re-adds it at the top of the stack.
+		remove(infoText);
+		insert(100, infoText);
 
 		// Resets shapePoint location back to the initial bit.
 		shapePointA.set(shapeCenterCoordinate.x, shapeCenterCoordinate.y - shapeRadius);
 		if (drawMode != 1)
 		{
 			shapePointB.set(shapePointA.x, shapePointA.y);
+			shapePointB.rotate(shapeCenterCoordinate, 360 / shapeSides);
 		}
 		else
 		{
 			shapePointB.set(shapeCenterCoordinate.x + shapeRadius);
 		}
 		isDrawing = true;
+
+		// Reset variables.
+		drawIterations = 0;
+
+		// Start the timer again.
+		if (quickDraw == false)
+		{
+			new FlxTimer().start(1 / shapeSides, timerComplete, 0);
+		}
+		else
+		{
+			new FlxTimer().start(0.25 / shapeSides, timerComplete, 0);
+		}
+	}
+
+	function setLineColour(colour:Int)
+	{
+		trace("setLineColour(" + colour + ")");
+		if (colour == 0)
+		{
+			lineStyle.color.setRGB(255, 255, 255);
+		}
+		else if (colour == 1) // R
+		{
+			lineStyle.color.setRGB(255, 0, 0);
+		}
+		else if (colour == 2) // O
+		{
+			lineStyle.color.setRGB(255, 127, 0);
+		}
+		else if (colour == 3) // Y
+		{
+			lineStyle.color.setRGB(255, 255, 0);
+		}
+		else if (colour == 4) // G
+		{
+			lineStyle.color.setRGB(0, 255, 0);
+		}
+		else if (colour == 5) // B
+		{
+			lineStyle.color.setRGB(0, 0, 255);
+		}
+		else if (colour == 6) // I
+		{
+			lineStyle.color.setRGB(75, 0, 130);
+		}
+		else if (colour == 7) // V
+		{
+			lineStyle.color.setRGB(148, 0, 211);
+		}
 	}
 }
